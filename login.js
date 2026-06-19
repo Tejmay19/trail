9289fdf9632c51927d57e38cbef894b60bd19f6d
Tejmay Kisan <163103836+Tejmay19@users.noreply.github.com>
2026-06-19 22:12:44 +0530

    Add Firebase initialization in login.js

---
+++ login.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyC1DyyBiA3RiZ3f_U1jkA1P6AAgI0Kh3sk",
+    authDomain: "loginpage-3a9c0.firebaseapp.com",
+    projectId: "loginpage-3a9c0",
+    storageBucket: "loginpage-3a9c0.firebasestorage.app",
+    messagingSenderId: "818275777736",
+    appId: "1:818275777736:web:0c016a1672e4bab2e9f9fd",
+    measurementId: "G-ZXXHJPN8PH"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
